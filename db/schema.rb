806584a3db8ec84f8e820dbb36c09238f1281830
Ian Lavoie <habicus@Ians-MacBook-Pro.local>
2017-11-30 14:04:12 -0500

    prep for heroku 14

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130184838) do
+ActiveRecord::Schema.define(version: 20171130190112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20171130184838) do
     t.string "content_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "viridians", force: :cascade do |t|
