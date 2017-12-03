a5fb69a5f5014348a72f8e7618b4b31fd5b81a86
Ian Lavoie <habicus@Ians-MacBook-Pro.local>
2017-12-03 16:29:16 -0500

    prep for heroku 17

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,10 @@
-class CreatePhotos < ActiveRecord::Migration[5.1]
-  def change
-    create_table :photos do |t|
-		t.string :title
-		t.string :content_type
-		t.timestamps
-    end
-  end
-  validates_attachment :image, content_type: {content_type: ["image/jpeg", "image/gif", "image/png"]}
-end
+# class CreatePhotos < ActiveRecord::Migration[5.1]
+#   # def change
+#   #   create_table :photos do |t|
+# 	# 	t.string :title
+# 	# 	t.string :content_type
+# 	# 	t.timestamps
+#   #   end
+#   # end
+#   # validates_attachment :image, content_type: {content_type: ["image/jpeg", "image/gif", "image/png"]}
+# end
